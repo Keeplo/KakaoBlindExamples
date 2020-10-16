1a561e493bd4109ed9cb3bcf7e47e419b283c363
YongwooKim <keepingitflow@gmail.com>
2020-10-16 16:40:00 +0900

    docs: Create the problem project

--- KakaoBlindLevel1/SecretMap/main.swift
+++ KakaoBlindLevel1/SecretMap/main.swift
@@ -5,7 +5,18 @@
 //  Created by Yongwoo Marco on 2020/10/16.
 //
 
+// 비밀지도 (1차)
+
+// https://programmers.co.kr/learn/courses/30/lessons/17681
+// 해설 : https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+// 정리 : 
+
 import Foundation
 
-print("Hello, World!")
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    return answer
+}
+
+let
 
