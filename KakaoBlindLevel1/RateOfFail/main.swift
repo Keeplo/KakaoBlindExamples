b760d679f293df4e95ed35f6803b02b545ef2dec
YongwooKim <keepingitflow@gmail.com>
2020-10-19 16:08:00 +0900

    docs: Made the project of this problem

---
+++ KakaoBlindLevel1/RateOfFail/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  RateOfFail
+//
+//  Created by Yongwoo Marco on 2020/10/19.
+//
+
+// 실패율 2019 Recruitment
+
+// https://programmers.co.kr/learn/courses/30/lessons/42889
+// 해설 : 
+// 정리 :
+
+
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    return []
+}
+
