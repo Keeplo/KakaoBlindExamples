862c191dad67e546972880f6949eda8315689871
YongwooKim <keepingitflow@gmail.com>
2020-10-30 14:53:23 +0900

    docs: Create the folder of the project

---
+++ KakaoBlindLevel1/Crane/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  Crane
+//
+//  Created by Yongwoo Marco on 2020/10/30.
+//
+
+// 크레인 인형뽑기 게임 2019 겨울 인턴쉽
+
+// https://programmers.co.kr/learn/courses/30/lessons/64061
+// 해설 :
+// 정리 :
+
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    return 0
+}
