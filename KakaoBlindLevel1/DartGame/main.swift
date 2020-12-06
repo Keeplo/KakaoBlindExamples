d1e80d246cd8ff92c346ded814bf2f8b1b69663c
YongwooKim <keepingitflow@gmail.com>
2020-12-06 17:45:39 +0900

    docs: DartGame 정리

--- KakaoBlindLevel1/DartGame/main.swift
+++ KakaoBlindLevel1/DartGame/main.swift
@@ -9,65 +9,66 @@
 
 // https://programmers.co.kr/learn/courses/30/lessons/17682
 // 해설 : https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
-// 정리 :
+// 정리 : https://keeplo.tistory.com/156
 
 import Foundation
 
-/*
-func solution(_ dartResult:String) -> Int {
-    var num = 0
-    var result = [Int]()
-    for (index, element) in dartResult.enumerated() {
-        switch element {
-        case "1":
-            if(Array(dartResult)[index+1]=="0") {
-                result.append(num)
-                num = -1
-            } else {
-                fallthrough
-            }
-        case "0":
-            if num == -1 {
-                num = 10
-            } else {
-                fallthrough
-            }
-        default:
-            if let intNum = Int(String(element)) {
-                result.append(num)
-                num = intNum
-            } else {
-                switch element {
-                case "S":
-                    break
-                case "D":
-                    num *= num
-                case "T":
-                    num *= num*num
-                case "*":
-                    result[result.count-1] *= 2
-                    num *= 2
-                case "#":
-                    num *= -1
-                default:
-                    print("error")
-                }
-            }
-        }
-    }
-    result.append(num)
-    
-    return result.reduce(0, +)
-}
 
- */
+//func solution(_ dartResult:String) -> Int {
+//    var num = 0
+//    var result = [Int]()
+//    for (index, element) in dartResult.enumerated() {
+//        switch element {
+//        case "1":
+//            if(Array(dartResult)[index+1]=="0") {
+//                result.append(num)
+//                num = -1
+//            } else {
+//                fallthrough
+//            }
+//        case "0":
+//            if num == -1 {
+//                num = 10
+//            } else {
+//                fallthrough
+//            }
+//        default:
+//            if let intNum = Int(String(element)) {
+//                result.append(num)
+//                num = intNum
+//            } else {
+//                switch element {
+//                case "S":
+//                    break
+//                case "D":
+//                    num *= num
+//                case "T":
+//                    num *= num*num
+//                case "*":
+//                    result[result.count-1] *= 2
+//                    num *= 2
+//                case "#":
+//                    num *= -1
+//                default:
+//                    print("error")
+//                }
+//            }
+//        }
+//    }
+//    result.append(num)
+//
+//    return result.reduce(0, +)
+//}
+
 func solution(_ dartResult:String) -> Int {
     let numbers = dartResult.split(whereSeparator: {$0.isLetter||$0=="*"||$0=="#"})
     let letters = dartResult.split(whereSeparator: {$0.isNumber})
-    
+    print(numbers)
+    print(letters)
+
     var result = numbers.map({Int($0)!})
     print(result)
-    
+
     for (index, element) in letters.enumerated() {
         for c in String(element) {
             let i = Int(index)
@@ -90,10 +91,11 @@ func solution(_ dartResult:String) -> Int {
             }
         }
     }
-    
+
     return result.reduce(0, +)
 }
 
-let dartResult = "1D2S#10S"
+//let dartResult = "1D2S#10S"
+//
+//print(solution(dartResult))
 
-print(solution(dartResult))
