6c519154a80fffa3046704a970b6c31ca049fdb8
YongwooKim <keepingitflow@gmail.com>
2020-12-10 22:46:55 +0900

    docs: confirm the problem

--- KakaoBlindLevel1/SecretMap/main.swift
+++ KakaoBlindLevel1/SecretMap/main.swift
@@ -49,14 +49,6 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     return answer
 }
 
-/*
-func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    var answer: [String] = []
-    
-    
-    return answer
-}*/
-
 let n = 5
 let arr1 = [9, 20, 28, 18, 11]
 let arr2 = [30, 1, 21, 17, 28]
