dc244444d7941abe6f26a058b347da4189d7bb11
YongwooKim <keepingitflow@gmail.com>
2020-12-11 22:14:20 +0900

    docs: Tried the better way

--- KakaoBlindLevel1/SecretMap/main.swift
+++ KakaoBlindLevel1/SecretMap/main.swift
@@ -13,52 +13,62 @@
 
 import Foundation
 
-
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-    var answer: [String] = []
+//    var answer: [String] = []
     
-    let arrA = arr1.map({ (i: Int) -> Int in
-        var changedNum = String(i, radix: 2)
-        while(n-changedNum.count > 0) {
-            changedNum = "0" + changedNum
-        }
-        return Int(changedNum)!
-    })
-    let arrB = arr2.map({ (i: Int) -> Int in
-        var changedNum = String(i, radix: 2)
-        while(n-changedNum.count > 0) {
-            changedNum = "0" + changedNum
-        }
-        return Int(changedNum)!
-    })
+//    let arrA = arr1.map({ (i: Int) -> String in
+//        var changedNum = String(i, radix: 2)
+//        while(n-changedNum.count > 0) {
+//            changedNum = "0" + changedNum // 맨앞에 0이 생략된 경우
+//        }
+//        return changedNum
+//    })
+//    let arrB = arr2.map({ (i: Int) -> String in
+//        var changedNum = String(i, radix: 2)
+//        while(n-changedNum.count > 0) {
+//            changedNum = "0" + changedNum // 맨앞에 0이 생략된 경우
+//        }
+//        return changedNum
+//    })
     
-    for i in 0..<n {
-        answer.append(String(arrA[i]|arrB[i]))
+//    for i in 0..<n {
+//        var midAnswer = [String]()
+//        for k in 0..<n {
+//            Array(Array(arrA)[i])[k] == Array(Array(arrB)[i])[k] ? Array(Array(arrA)[i])[k] == "1" ? midAnswer.append("#") : midAnswer.append(" ") : midAnswer.append("#")
+//        }
+//        answer.append(midAnswer.reduce("", +))
+//    }
+    let answer = (0..<n).map{ (i: Int) -> String in
+        var data = String(UInt16(arr1[i]) | UInt16(arr2[i]))
+        while(n-data.count > 0) {
+            data = "0" + data // 맨앞에 0이 생략된 경우
+        }
+        
+        return data
     }
+    print(answer)
     
-    /*
-    for i in 0..<n {
-        var midAnswer = [String]()
-        for k in 0..<n {
-            Array(Array(arrA)[i])[k] == Array(Array(arrB)[i])[k] ? Array(Array(arrA)[i])[k] == "1" ? midAnswer.append("#") : midAnswer.append(" ") : midAnswer.append("#")
-        }
-        answer.append(midAnswer.reduce("", +))
-    }
-    */
+    let result = answer.map({ $0.reduce("") { $1 == "1" ? $0 + "#" : $0 + " " } })
     
-    return answer
+    return result
 }
 
+//ex1
 let n = 5
 let arr1 = [9, 20, 28, 18, 11]
 let arr2 = [30, 1, 21, 17, 28]
 
+//ex2
+//let n = 6
+//let arr1 = [46, 33, 33 ,22, 31, 50]
+//let arr2 = [27 ,56, 19, 14, 14, 10]
+
 print(solution(n, arr1, arr2))
 
 
-//let a = 21
-//let b = 13
-//
+//let a = 4 // 100
+//let b = 3 // 11
+
 //let aa = Int8(a)
 //let bb = String(b, radix: 2)
 //
@@ -70,3 +80,10 @@ print(solution(n, arr1, arr2))
 //let aaa = UInt8(bigEndian: UInt8(aa))
 //
 //print(aaa)
+//
+//print()
+//
+//let a = 9 // 1001
+//let b = 7 // 111
+//
+//print(UInt16(a) | UInt16(b))
