225e4d5189176b45ede968346be06f457ff42914
YongwooKim <keepingitflow@gmail.com>
2020-12-15 22:52:37 +0900

    docs: Post the problem

--- KakaoBlindLevel1/SecretMap/main.swift
+++ KakaoBlindLevel1/SecretMap/main.swift
@@ -9,43 +9,14 @@
 
 // https://programmers.co.kr/learn/courses/30/lessons/17681
 // 해설 : https://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
-// 정리 : 
+// 정리 : https://keeplo.tistory.com/159
 
 import Foundation
-
-//func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
-//    var answer: [String] = []
-    
-//    let arrA = arr1.map({ (i: Int) -> String in
-//        var changedNum = String(i, radix: 2)
-//        while(n-changedNum.count > 0) {
-//            changedNum = "0" + changedNum // 맨앞에 0이 생략된 경우
-//        }
-//        return changedNum
-//    })
-//    let arrB = arr2.map({ (i: Int) -> String in
-//        var changedNum = String(i, radix: 2)
-//        while(n-changedNum.count > 0) {
-//            changedNum = "0" + changedNum // 맨앞에 0이 생략된 경우
-//        }
-//        return changedNum
-//    })
-    
-//    for i in 0..<n {
-//        var midAnswer = [String]()
-//        for k in 0..<n {
-//            Array(Array(arrA)[i])[k] == Array(Array(arrB)[i])[k] ? Array(Array(arrA)[i])[k] == "1" ? midAnswer.append("#") : midAnswer.append(" ") : midAnswer.append("#")
-//        }
-//        answer.append(midAnswer.reduce("", +))
-//    }
 
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     let answer = (0..<n).map{ (i: Int) -> String in
         var data = String(UInt16(arr1[i]) | UInt16(arr2[i]), radix: 2)
-        while(n-data.count > 0) {
-            data = "0" + data // 맨앞에 0이 생략된 경우
-        }
-        return data
+        return n-data.count > 0 ? String(repeating: "0", count: n-data.count) + data : data
     }
     
     let result = answer.map({ $0.reduce("") { $1 == "1" ? $0 + "#" : $0 + " " } })
