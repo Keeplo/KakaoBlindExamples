de5e957b51c7c10084c12af5c62cba2197ce9f20
YongwooKim <keepingitflow@gmail.com>
2020-12-15 20:47:09 +0900

    소스트리 연습 1

--- KakaoBlindLevel1/SecretMap/main.swift
+++ KakaoBlindLevel1/SecretMap/main.swift
@@ -53,12 +53,12 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     return result
 }
 
-//ex1
+// Test 1
 let n = 5
 let arr1 = [9, 20, 28, 18, 11]
 let arr2 = [30, 1, 21, 17, 28]
 
-//ex2
+// Test 2
 //let n = 6
 //let arr1 = [46, 33, 33 ,22, 31, 50]
 //let arr2 = [27 ,56, 19, 14, 14, 10]
