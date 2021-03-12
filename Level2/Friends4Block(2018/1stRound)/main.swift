dd207832c5055e78118b48149d843e8f89cce071
YongwooKim <keepingitflow@gmail.com>
2021-03-12 20:42:35 +0900

    Docs : Solved "프렌즈4블록"

--- Level2/Friends4Block(2018/1stRound)/main.swift
+++ Level2/Friends4Block(2018/1stRound)/main.swift
@@ -13,73 +13,87 @@
 import Foundation
 
 func solution(_ m:Int, _ n:Int, _ board:[String]) -> Int {
-    var b = board.map{ $0.map{ (String($0), 0) } }
-    let d = [(0, 0), (1, 0), (0, 1), (1, 1)]
-        
-    print(b)
+    var b = board.map{ $0.map{ (String($0), 0) } }, isMove = true
+    let d = [(0, 0), (1, 0), (0, 1), (1, 1)] // y, x
     
-    func check(_ x: Int,_ y: Int) {
-        print("x \(x) y \(y)")
-        if x < m-1, y < n-1 {
-            // 맵 내부
-            check(x+1, y)
-            check(x, y+1)
-            
-            if b[y][x].0 == b[y+1][x].0, b[y][x].0 == b[y][x+1].0, b[y][x].0 == b[y+1][x+1].0 {
-                for i in d {
-                    b[y+i.0][x+i.1].1 += 1
-                }
-            }
-        } else {
-            // 맵 외부
-        }
-       
-        
-        if x == 0, y == 0 {
-            remove()
-            move()
-        }
-    }
-    
-    func move() {
-        for y in 0..<b.count {
-            for x in 0..<b[0].count {
-                if y < b.count - 1 {
-                    if b[y+1][x].0 == "1" {
-                        b[y+1][x] = b[y][x]
-                        b[y][x].0 = "0"
+    func check(_ arr: inout [[(String, Int)]]) {
+        for y in 0..<arr.count {
+            for x in 0..<arr.first!.count {
+                if y < arr.count - 1, x < arr.first!.count - 1 {
+                    if arr[y+1][x].0 == arr[y][x].0, arr[y][x+1].0 == arr[y][x].0, arr[y][x].0 == arr[y+1][x+1].0, arr[y][x].0 != "0" {
+                        for (j, i) in d {
+                            arr[y+j][x+i].1 += 1
+                        }
                     }
                 }
             }
         }
+//        print("check - - - - - - - - - ")
+//        for bb in arr {
+//            print(bb)
+//        }
+        remove(&arr)
     }
-    func remove() {
-        for y in 0..<b.count {
-            for x in 0..<b[0].count {
-                if y < b.count - 1 {
-                    if b[y+1][x].1 > 0 {
-                        b[y+1][x].0 = "1"
+    func remove(_ arr: inout [[(String, Int)]]) {
+        for y in 0..<arr.count {
+            for x in 0..<arr.first!.count {
+                if arr[y][x].1 > 0 {
+                    arr[y][x] = ("0", 0)
+                }
+            }
+        }
+//        print("removed")
+//        for bb in arr {
+//            print(bb)
+//        }
+        isMove = false
+        move(&arr)
+    }
+    func move(_ arr: inout [[(String, Int)]]) {
+        for x in 0..<arr.first!.count {
+            for y in 0..<arr.count-1 {
+                if arr[y][x].0 != "0", arr[y+1][x].0 == "0" {
+                    isMove = true
+                    for z in (1...y+1).reversed() {
+                        arr[z][x] = arr[z-1][x]
+                        arr[z-1][x] = ("0", 0)
                     }
                 }
             }
         }
+//        print("move")
+//        for bb in arr {
+//            print(bb)
+//        }
     }
-
-    check(0, 0)
     
-    return b.reduce(0){ $0 + $1.filter{ $0.0 == "0" }.count }
+    while isMove {
+        check(&b)
+    }
+//
+//    for bb in b {
+//        print(bb)
+//    }
+    
+    return b.reduce(0, { a, b in
+        return a + b.filter({ $0.0 == "0" }).count
+    })
 }
 
 
 
-let m = 4
-let n = 5
-let board = ["CCBDE", "AA1DE", "AA1BF", "CC1BF"]   // 14
+//let m = 4
+//let n = 5
 //let board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]   // 14
 
 
-//let m = 6
-//let n = 6
-//let borad = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]   // 15
+let m = 6
+let n = 6
+let board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]   // 15
+
+
+//let m = 2
+//let n = 2
+//let board = ["TTT", "TTT"]   // 4
 
 print(solution(m, n, board))
