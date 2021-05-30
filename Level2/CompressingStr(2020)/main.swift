80d3807cc2f14c0878064b3796a5fa833530552c
YongwooKim <keepingitflow@gmail.com>
2021-05-30 20:30:52 +0900

    fix : 커밋 닫기
    님ㄴ마ㅡㄴ이ㅡㅏㅁ늘
    
    closed #3

--- Level2/CompressingStr(2020)/main.swift
+++ Level2/CompressingStr(2020)/main.swift
@@ -16,41 +16,50 @@ func solution(_ s:String) -> Int {
     if s.count == 1 { return 1 }
     var lenghs = [Int](repeating: 0, count: s.count/2+1), str = s, stack = [String]()
     
-    lenghs[0] = s.count
     
-    for i in 1...s.count/2 {
-        print("i \(i)")
-        var q = str
-        var result = ""
-        while q.count > 0 {
-            let range = q.startIndex..<q.index(q.startIndex, offsetBy: i)
-            
-            if let last = stack.last, last != String(q[range]) {
-                print("last \(last), q \(q[range])")
-                
-                if stack.count > 1 {
-                    result.append("\(stack.count)\(stack.last!)")
-                    stack.removeAll()
-                } else {
-                    result.append("\( String(stack.last![stack.last!.startIndex..<stack.last!.index(stack.last!.startIndex, offsetBy: i)]) )")
-                    q = String(stack.last![stack.last!.index(stack.last!.startIndex, offsetBy: i)..<stack.last!.endIndex]) + q
-                    stack.removeAll()
-                }
-            } else {
-                stack.append( String(q[range]) )
-                q.removeFirst(i)
-            }
-        }
-        if stack.count > 0 {
-            result.append("\(stack.count)\(stack.last!)")
-            stack.removeAll()
-        }
-        print(result)
-        lenghs[i] = result.count
-    }
     
     return lenghs.min()!
 }
+
+//func solution(_ s:String) -> Int {
+//    if s.count == 1 { return 1 }
+//    var lenghs = [Int](repeating: 0, count: s.count/2+1), str = s, stack = [String]()
+//
+//    lenghs[0] = s.count
+//
+//    for i in 1...s.count/2 {
+//        print("i \(i)")
+//        var q = str
+//        var result = ""
+//        while q.count > 0 {
+//            let range = q.startIndex..<q.index(q.startIndex, offsetBy: i)
+//
+//            if let last = stack.last, last != String(q[range]) {
+//                print("last \(last), q \(q[range])")
+//
+//                if stack.count > 1 {
+//                    result.append("\(stack.count)\(stack.last!)")
+//                    stack.removeAll()
+//                } else {
+//                    result.append("\( String(stack.last![stack.last!.startIndex..<stack.last!.index(stack.last!.startIndex, offsetBy: i)]) )")
+//                    q = String(stack.last![stack.last!.index(stack.last!.startIndex, offsetBy: i)..<stack.last!.endIndex]) + q
+//                    stack.removeAll()
+//                }
+//            } else {
+//                stack.append( String(q[range]) )
+//                q.removeFirst(i)
+//            }
+//        }
+//        if stack.count > 0 {
+//            result.append("\(stack.count)\(stack.last!)")
+//            stack.removeAll()
+//        }
+//        print(result)
+//        lenghs[i] = result.count
+//    }
+//
+//    return lenghs.min()!
+//}
 
 let s = "aabbaccc"                      //7
 //let s = "ababcdcdababcdcd"              // 9
